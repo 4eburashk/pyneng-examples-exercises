2c7de5829b4e79edf8eb849ceb985b0c209f9ddd
4eburashk <csn@net4me.net>
2018-06-07 01:48:34 +0300

    4.4 done

--- exercises/04_data_structures/task_4_4.py
+++ exercises/04_data_structures/task_4_4.py
@@ -14,3 +14,15 @@
 
 command1 = 'switchport trunk allowed vlan 1,3,10,20,30,100'
 command2 = 'switchport trunk allowed vlan 1,3,100,200,300'
+vlans1 = (command1.split(' ')[-1]).split(',')
+vlans2 = (command2.split(' ')[-1]).split(',')
+print(vlans1, vlans2) # собрали вланы в листы
+vlans1 =  [ int(vlan) for vlan in vlans1 if vlan.isdigit() ]
+vlans2 =  [ int(vlan) for vlan in vlans2 if vlan.isdigit() ]
+print(vlans1, vlans2) # взяли из стрингов циферки (про map мы пока не знаем)
+set1 = set(vlans1)
+set2 = set(vlans2)
+print(set1, set2) # сравнивать мы умеем множества.
+total = list(set1 & set2) # но в итоге по заданию нам нужен именно список.
+print(total)
+
