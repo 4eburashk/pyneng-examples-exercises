534bdf433ae878b99d6d4647c63083ead523b37e
4eburashk <csn@net4me.net>
2018-06-07 20:52:30 +0300

    4.8 done

--- exercises/04_data_structures/task_4_8.py
+++ exercises/04_data_structures/task_4_8.py
@@ -19,3 +19,9 @@
 '''
 
 IP = '192.168.3.1'
+ip = IP.replace('.',",").split(',')
+ip_template = '''
+{0:<10} {1:<10} {2:<10} {3:<10}
+{0:08b}   {1:08b}   {2:08b}   {3:08b}
+'''
+print(ip_template.format(int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3])))
