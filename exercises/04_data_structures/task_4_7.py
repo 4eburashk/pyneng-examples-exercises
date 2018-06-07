f70e81997e8619fc98a0de54d25dab5487ad4e22
4eburashk <csn@net4me.net>
2018-06-07 19:10:23 +0300

    4.7 done

--- exercises/04_data_structures/task_4_7.py
+++ exercises/04_data_structures/task_4_7.py
@@ -9,3 +9,11 @@
 '''
 
 MAC = 'AAAA:BBBB:CCCC'
+mac_16 = MAC.replace(':',",").split(',')
+print(mac_16)
+mac_int = [ int(m,16) for m in mac_16 ]
+print(mac_int)
+mac_bin = '{:b} {:b} {:b}'.format(mac_int[0],mac_int[1],mac_int[2])
+print(mac_bin)
+
+
